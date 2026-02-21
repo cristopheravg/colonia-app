42c5c997464a96a902d8aef80a7112882dc3f8f0
Cristopher Avila <cristopher.avilag@gmail.com>
2026-02-21 14:57:18 -0600

    Limitador de memoria: cambio a ES modules

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   apps : [{
     name: "colonia-backend",
-    cwd: "/var/www/colonia-app/backend",  // Directorio de trabajo
-    script: "src/app.js",                   // Ruta completa al archivo
+    cwd: "/var/www/colonia-app/backend",
+    script: "src/app.js",
     node_args: "--max-old-space-size=512",
     max_memory_restart: "600M",
     env: {
